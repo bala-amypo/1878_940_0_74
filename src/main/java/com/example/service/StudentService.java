495f227e568e5f0f82e1a5eb290c927d8164ef45
bala-amypo <bala@amypo.in>
2025-12-17 09:53:49 +0000

    User commit

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -1,10 +1,14 @@
-package com.example.demoo.service;
+package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Student;
+
 @Service
-public class studentService{
-
-
+public interface StudentService {
+    
+    Student saveStudent(Student student);// Creating object for saving student details and class
+    
+ 
     
 }
